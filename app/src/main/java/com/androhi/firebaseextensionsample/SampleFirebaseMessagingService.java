f14371e7da2923aeaa626343abaae48e5beb5c5c
Shimokawa Takahiro <takahiro.1828@gmail.com>
2016-05-27 00:43:19 +0900

    Add Firebase Notifications

---
+++ app/src/main/java/com/androhi/firebaseextensionsample/SampleFirebaseMessagingService.java
@@ -0,0 +1,15 @@
+package com.androhi.firebaseextensionsample;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by androhi on 16/05/26.
+ */
+public class SampleFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+    }
+}
